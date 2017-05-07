0888fcd90df20737f812ce110497d697542a1b3c
chithra <chithra.venkatesan.5@gmail.com>
2017-05-07 09:47:29 -0400

    Add port to app.js for production

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
 
 var express = require('express'); // Express web server framework
 var app = express();
+var port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
-console.log('Listening on 8080');
-app.listen(8080);
+console.log(`Listening on ${port}`);
+app.listen(port);
